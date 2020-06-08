d4b521b7467d31c7dee454284ec286dba8a3d127
Yuzhe <yiyuzhe0125@outlook.com>
2020-06-08 14:36:01 -0600

    finished section 1.2

--- chapter1/section1.2/beads.cpp
+++ chapter1/section1.2/beads.cpp
@@ -1,7 +1,17 @@
 /*
 ID: yiyuzhe1
-TASK: friday
+TASK: beads
 LANG: C++                 
+*/
+
+/*
+ I have to admit that I got stuck on this questioan again, 
+ and took the example solution as a reference to get this
+ solution. And this solution takes O(n^2), though I believe
+ this question can be done in O(n), it seems really complicated.
+ Luckily, the question doesn't have running time limits. So
+ hopefully I can get a solution with O(n) running time one day
+ :c
 */
 
 #include <iostream>
@@ -13,30 +23,47 @@ using namespace std;
 int main(void){
     ifstream fin("beads.in");
     ofstream fout("beads.out");
-    unsigned int n, curPos, nextPos;
+    unsigned int n;
     string beads;
 
     fin >> n >> beads;
     beads += beads;
-    
-    //find the first not white bead
-    string curBeads;
-    for(int i = 0; i < n; ++i)
-        if(beads[i] != 'w'){
-            curBeads[0] = beadds[i];
-            curPos = i;
-        }
 
-    //if all beads are white
-    if(curBeads[0] != 'r' && curBeads[0] != 'b'){
-        fout << n;
-
-        return 0;
-    }
-
+    unsigned int state;
     unsigned int max = 0;
 
     for(int i = 0; i < 2 * n; ++i){
-        
+        if(beads[i] == 'w')
+            state  = 0;
+        else 
+            state = 1;
+
+        int j = i + 1;
+        unsigned int length = 1;
+        string color;
+        color.push_back(beads[i]);
+
+        while(j < n * 2){
+            if(color[0] != beads[j] && beads[j] != 'w'){
+                ++state;
+                color[0] = beads[j];
+            }
+
+            if(state > 2)
+                break;
+            
+            ++length;
+            ++j;
+        }
+
+        if(length > max)
+            max = length;
+
+        if(max >  n)
+            max = n;
     }
+
+    fout << max << endl;
+
+    return 0;
 }
